b53217e64a19f2205804f49417e6f8c7be2dc96c
Trias <triasbrata@gmail.com>
2024-10-01 10:07:01 +0700

    nextron

--- src/renderer/components/magicui/safari-demo.tsx
+++
@@ -1,9 +0,0 @@
-import Safari from '@components/magicui/safari';
-
-export default function SafariDemo() {
-  return (
-    <div className='relative'>
-      <Safari url='magicui.design' className='size-full' />
-    </div>
-  );
-}
